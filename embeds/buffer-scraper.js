7d41d707323e010f1309fe2cac4aad6e26a15ad7
Tom Ashworth <tom@phuu.net>
2012-05-21 14:35:42 +0100

    JS scraper v1

---
+++ embeds/buffer-scraper.js
@@ -0,0 +1,182 @@
+;(function () {
+
+    console.log("Filtering images.");
+
+    // Utility functions
+    function rtrim (str, charlist) {
+      // Removes trailing whitespace
+      //
+      // version: 1109.2015
+      // discuss at: http://phpjs.org/functions/rtrim
+      // +   original by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+      // +      input by: Erkekjetter
+      // +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+      // +   bugfixed by: Onno Marsman
+      // +   input by: rem
+      // +   bugfixed by: Brett Zamir (http://brett-zamir.me)
+      // *     example 1: rtrim('    Kevin van Zonneveld    ');
+      // *     returns 1: '    Kevin van Zonneveld'
+      charlist = !charlist ? ' \\s\u00A0' : (charlist + '').replace(/([\[\]\(\)\.\?\/\*\{\}\+\$\^\:])/g, '\\$1');
+      var re = new RegExp('[' + charlist + ']+$', 'g');
+      return (str + '').replace(re, '');
+    }
+
+    // Info
+    var scraper = {};
+    scraper.base = document.location.origin;
+    scraper.path = document.location.pathname;
+    scraper.url = document.location.href;
+
+    var config = {};
+    config.banned = [
+        'http://ds.issuemediagroup.com'
+    ];
+    config.need = 8;
+
+    var formatSrc = function (src) {
+
+        // add protocol to relative urls
+        if( src.substring(0, 2) === "//" ) {
+            src = document.location.protocol + src;
+        }
+
+        // prepend site if missing
+        if( ! src.match(/^https?:\/\//g) ) {
+
+            // relative to site root
+            if( src.substring(0,1) === "/" ) {
+                src = scraper.url + src;
+            }
+            // no slash, relative to base
+            else {
+                src = scraper.base + scraper.path + '/' + src;
+            }
+
+        }
+
+        return src;
+
+    };
+
+    var calcGCD = function(size) {
+        var a = size.x, b = size.y;
+        var remander = 0;
+        while (b !== 0) {
+            remainder = a % b;
+            a = b;
+            b = remainder;
+        }
+        return Math.abs(a);
+    };
+
+    var calcAspect = function(size) {
+        if ( ! size.x || ! size.y ) return 1;
+        var gcd = calcGCD(size);
+        return (size.x / gcd) / (size.y / gcd);
+    };
+
+    // Filter out unwanted images
+    var filterImage = function (img) {
+
+        var src, ext, height = $(img).width(), width = $(img).height(), aspect;
+
+        src = $(img).attr('src');
+
+        ext = src.split('.').pop(); // This could break with query string... kitten.jpg?size=large
+
+        src = formatSrc(src);
+
+        // valid extension?
+        if( ! ext.match(/(jpg|jpeg|gif|png)/i) ) { return false; }
+
+        aspect = calcAspect({x: width, y: height});
+
+        if( height < 50 || width < 85 || aspect < 0.4 || aspect > 4 ) { return false; }
+
+        return {
+            url: src,
+            title: $(img).attr('title'),
+            height: height,
+            width: width
+        };
+
+    };
+    
+    // Retrieve & filter images from current page
+    var getImages = function () {
+
+        // Old-skool base tag
+        var base = $('base');
+        if( base.length > 0 ) {
+            scraper.base = rtrim($('base').first().attr('href'), '/') + '/';
+        }
+        
+        // All images
+        var images = $('img'), filtered = [], srcs = {};
+
+        // Grab the open graph image
+        var ogimage = $('meta[property="og:image"]');
+        var ogtmp;
+        if( ogimage.length > 0 ) {
+            ogtmp = $('<img>').prop({
+                'src': $(ogimage).text(),
+                'class': 'opengraph',
+                'width': 1000, // High priority
+                'height': 1000
+            });
+            images.push(ogtmp);
+        }
+
+        // Cycle through all images
+        var i = 0, l=images.length, result, img;
+        for(; i < l; i++) {
+
+            img = images[i];
+
+            // Have we seen this image already?
+            if( !! srcs[$(img).attr('src')] ) {
+                // Yep, skip it
+                continue;
+            } else {
+                // Nope, remember it
+                srcs[$(img).attr('src')] = true;
+            }
+
+            result = filterImage(img);
+
+            if( result !== false ) {
+                filtered.push(result);
+            }
+
+        }
+
+        var pow = Math.pow;
+        filtered.sort(function (a, b) {
+            return pow(pow(b.width, 2) + pow(b.height, 2), 1/2) - pow(pow(a.width, 2) + pow(a.height, 2), 1/2);
+        });
+
+        console.log(filtered);
+
+        return filtered.slice(0, config.need);
+
+    };
+
+    var getDescription = function () {
+        var text = $('p').first().text().substring(0, 200);
+        return $('meta[property="og:description"]').text() || $('meta[name=description]').prop('content') || (text.length > 0 ? text + '...' : false) || '';
+    };
+
+    var getDetails = function () {
+
+        return {
+            original: scraper.url,
+            title: $('meta[property="og:title"]').prop('content') || document.title || $('h1').first().text() || '',
+            description: getDescription(),
+            images: getImages()
+        };
+
+    };
+
+    console.log(getDetails());
+    
+}());
