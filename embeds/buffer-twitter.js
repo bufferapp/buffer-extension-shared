dd1c76efc673390cf43686d4e481f3e00a97e1b3
Tom Ashworth <tom@phuu.net>
2012-05-29 14:11:38 +0100

    FIxed: duplicate tweet syndrome + turned off local!

--- embeds/buffer-twitter.js
+++ embeds/buffer-twitter.js
@@ -254,18 +254,25 @@
             },
             data: function (elem) {
                 var c = $(elem).closest('.tweet');
-                var text = c.find('.js-tweet-text');
+                // Grab the tweet text
+                var text = c.find('.js-tweet-text').first();
+                // Iterate through all links in the text
                 $(text).children('a').each(function () {
+                    // Don't modify the screenames and the hastags
                     if( $(this).attr('data-screen-name') ) return;
                     if( $(this).hasClass('twitter-hashtag') ) return;
+                    // swap the text with the actual link
                     var original = $(this).text();
                     $(this).text($(this).attr("href")).attr('data-original-text', original);
                 });
+                // Build the RT text
                 var rt = 'RT ' + c.find('.username').text().trim() + ': ' + $(text).text() + '';
+                // Put the right links back
                 $(text).children('a').each(function () {
                     if( ! $(this).attr('data-original-text') ) return;
                     $(this).text($(this).attr('data-original-text'));
                 });
+                // Send back the data
                 return {
                     text: rt,
                     placement: 'twitter-permalink'
@@ -320,18 +327,25 @@
             },
             data: function (elem) {
                 var c = $(elem).closest('.tweet');
-                var text = c.find('.js-tweet-text');
+                // Grab the tweet text
+                var text = c.find('.js-tweet-text').first();
+                // Iterate through all links in the text
                 $(text).children('a').each(function () {
+                    // Don't modify the screenames and the hastags
                     if( $(this).attr('data-screen-name') ) return;
                     if( $(this).hasClass('twitter-hashtag') ) return;
+                    // swap the text with the actual link
                     var original = $(this).text();
                     $(this).text($(this).attr("href")).attr('data-original-text', original);
                 });
+                // Build the RT text
                 var rt = 'RT ' + c.find('.username').text().trim() + ': ' + $(text).text() + '';
+                // Put the right links back
                 $(text).children('a').each(function () {
                     if( ! $(this).attr('data-original-text') ) return;
                     $(this).text($(this).attr('data-original-text'));
                 });
+                // Send back the data
                 return {
                     text: rt,
                     placement: 'twitter-feed'
