596a57af367339fb0480096967624fb20c49a352
Tom Ashworth <tom@phuu.net>
2012-05-30 13:06:43 +0100

    I hate git.

--- embeds/buffer-overlay-scraper.js
+++ embeds/buffer-overlay-scraper.js
@@ -1,12 +1,25 @@
 ;(function () {
 
-  xt.port.on('buffer_details', function (details) {
-    var temp = document.createElement('div');
-    temp.setAttribute('id', 'page-scraper');
-    temp.setAttribute('data-details', escape(JSON.stringify(details)));
-    document.body.appendChild(temp);
-  });
+  // Only allow this to run in the Buffer iframe
+  if( window !== window.top && document.location.host.match(/bufferapp.com/i) ) {
 
-  xt.port.emit("buffer_details_request");
+    // Listen for information
+    xt.port.on('buffer_details', function (details) {
+
+      console.log("Overlay has details", details);
+
+      // Create an element in the overlay page with the
+      // details scraped externally
+      var temp = document.createElement('div');
+      temp.setAttribute('id', 'page-scraper');
+      temp.setAttribute('data-details', escape(JSON.stringify(details)));
+      document.body.appendChild(temp);
+
+    });
+
+    // Ask for the details
+    xt.port.emit("buffer_details_request");
+
+  }
 
 }());
