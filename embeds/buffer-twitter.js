d581da95e6c7235390209a9807d9cbe4b9fcd280
Niel de la Rouviere <nieldlr@gmail.com>
2013-11-05 13:10:00 +0800

    Change targeting of buffer inline embed to prevent odd coloring. Piggy-backing on another class. sm-embed wasn't working

--- embeds/buffer-twitter.js
+++ embeds/buffer-twitter.js
@@ -341,7 +341,7 @@
                 a.setAttribute('href', '#');
 
                 var i = document.createElement('i');
-                i.setAttribute('class', 'sm-embed'); // let Twitter set the bg colors
+                i.setAttribute('class', 'sm-reply'); // let Twitter set the bg colors
                 i.setAttribute('style', 'position: relative; top: 0px; margin-right: 4px; width: 13px; height: 13px; background-image: url(' + xt.data.get('data/shared/img/twttr-sprite-small.png') + ')!important; background-repeat: no-repeat;');
 
                 $(a).append(i);
