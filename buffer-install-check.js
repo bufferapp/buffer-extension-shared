747b865e4583cca7987f5f48cd85f6ba23dcc5b3
Toby Osbourn <toby.osbourn@googlemail.com>
2013-12-31 16:58:57 +0000

    The code is less IFFE now :-)
    I had initially wrapped the code to make it a self-executing anonumous
    function, but now I see better how the other extentions work along side
    this shared codebase I don't think that is necessary.

--- buffer-install-check.js
+++ buffer-install-check.js
@@ -2,19 +2,16 @@
 // (c) 2013 Buffer
 // Adds an element to our app page that we can use to check if the browser has our extension installed.
 
-;(function() {
-	
-	var bufferMarkOurSite = function (version) {
-		if (window.top !== window) return;
+var bufferMarkOurSite = function (version) {
+	if (window.top !== window) return;
 
-		if(document.location.host.match(/bufferapp.com/i)) {
-			var extensionInformation = document.createElement('div');
+	if(document.location.host.match(/bufferapp.com/i)) {
+		var extensionInformation = document.createElement('div');
 
-			extensionInformation.setAttribute('id', 'browser-extension-check');
-			extensionInformation.setAttribute('data-version', version);
+		extensionInformation.setAttribute('id', 'browser-extension-check');
+		extensionInformation.setAttribute('data-version', version);
 
-			document.body.appendChild(extensionInformation);
-		}
-	};
+		document.body.appendChild(extensionInformation);
+	}
+};
 
-}());
