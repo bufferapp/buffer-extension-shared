d7127ababb64edbe652064f6e0c3a086b2176d20
Dan Farrelly <daniel.j.farrelly@gmail.com>
2014-07-09 11:43:39 -0700

    Style adjustments to Quora script.

--- embeds/buffer-quora.js
+++ embeds/buffer-quora.js
@@ -1,154 +1,148 @@
 ;(function() {
 
-    var config = {};
-    config.buttons = [
-        {
-            text: 'Buffer',
-            container: '.item_action_bar',
-            after: '.share_link',
-            before: '.downvote',
-            containerClassName: 'buffer-quora-button-container',
-            separatorClassName: 'bullet',
-            separatorText: ' &bull; ',
-            className: 'buffer-quora-button',
-            data: function (element) {
+  var config = {};
+  config.buttons = [
+    {
+      text: 'Buffer',
+      container: '.item_action_bar',
+      after: '.share_link',
+      before: '.downvote',
+      containerClassName: 'buffer-quora-button-container',
+      separatorClassName: 'bullet',
+      separatorText: ' &bull; ',
+      className: 'buffer-quora-button',
+      data: function (element) {
 
-                var $parent = $(element).parents('.e_col'),
-                    $author,
-                    title,
-                    link;
+        var $parent = $(element).parents('.e_col'),
+          $author,
+          title,
+          link;
 
-                // We'll be using H1 tag to detect a question page
-                var $h1 = $('h1');
+        // We'll be using H1 tag to detect a question page
+        var $h1 = $('h1');
 
-                // Question page
-                if ($h1.length)
-                {
-                    // Find and sanitize a question title
-                    $h1.find('div').remove();
-                    title = $h1.text();
+        // Question page
+        if ($h1.length) {
+          // Find and sanitize a question title
+          $h1.find('div').remove();
+          title = $h1.text();
 
-                    // If it's an answer...
-                    $author = $parent.find('.answer_user_wrapper');
-                    if ($author.length)
-                    {
-                        title = 'Answer by ' + $author.find('a.user').text() + ' to ' + title;
-                    }
+          // If it's an answer...
+          $author = $parent.find('.answer_user_wrapper');
+          if ($author.length) {
+            title = 'Answer by ' + $author.find('a.user').text() + ' to ' + title;
+          }
 
-                    // Just grab the current page URL, we don't need to do voodoo here
-                    link = window.location.href;
+          // Just grab the current page URL, we don't need to do voodoo here
+          link = window.location.href;
 
-                }
-                // Home page feed
-                else
-                {
-                    // Find and sanitize a question title
-                    var $link_text = $parent.find('.link_text');
-                    $link_text.find('div').remove();
-                    $link_text.find('span').remove();
-                    title = $link_text.text();
+        // Home page feed
+        } else {
 
-                    // If it's an answer...
-                    $author = $parent.find('.feed_item_answer_user');
-                    if ($author.length)
-                    {
-                        title = 'Answer by ' + $author.find('a.user').text() + ' to ' + title;
-                    }
+          // Find and sanitize a question title
+          var $link_text = $parent.find('.link_text');
+          $link_text.find('div').remove();
+          $link_text.find('span').remove();
+          title = $link_text.text();
 
-                    // Prefix link with the domain URL as we only have an URI
-                    link = 'https://www.quora.com' + $parent.find('a.question_link').attr('href');
+          // If it's an answer...
+          $author = $parent.find('.feed_item_answer_user');
+          if ($author.length) {
+            title = 'Answer by ' + $author.find('a.user').text() + ' to ' + title;
+          }
 
-                }
+          // Prefix link with the domain URL as we only have an URI
+          link = 'https://www.quora.com' + $parent.find('a.question_link').attr('href');
 
-                return {
-                    text: title,
-                    url: link,
-                    placement: 'quora-action-bar'
-                };
-
-            }
         }
-    ];
 
-    var createButton = function (cfg)
-    {
+        return {
+          text: title,
+          url: link,
+          placement: 'quora-action-bar'
+        };
 
-        // Buffer link itself
-        var a = document.createElement('a');
-        // share_link is added for styling purposes
-        a.setAttribute('class', cfg.className + ' share_link');
-        a.setAttribute('href', '#');
-        $(a).text(cfg.text);
+      }
+    }
+  ];
 
-        // Quora style button separator
-        var bullet = document.createElement('span');
-        bullet.setAttribute('class', cfg.separatorClassName);
-        $(bullet).html(cfg.separatorText);
+  var createButton = function (cfg) {
 
-        // Container for button and separator
-        var c = document.createElement('span');
-        c.setAttribute('class', cfg.containerClassName);
-        $(c).append(bullet).append(a);
+    // Buffer link itself
+    var a = document.createElement('a');
+    // share_link is added for styling purposes
+    a.setAttribute('class', cfg.className + ' share_link');
+    a.setAttribute('href', '#');
+    $(a).text(cfg.text);
 
-        return c;
-    };
+    // Quora style button separator
+    var bullet = document.createElement('span');
+    bullet.setAttribute('class', cfg.separatorClassName);
+    $(bullet).html(cfg.separatorText);
 
-    var insertButtons = function ()
-    {
+    // Container for button and separator
+    var c = document.createElement('span');
+    c.setAttribute('class', cfg.containerClassName);
+    $(c).append(bullet).append(a);
 
-        config.buttons.forEach(function(btnConfig, i){
+    return c;
+  };
 
-            $(btnConfig.container).each(function(){
+  var insertButtons = function () {
 
-                var $container = $(this);
+    config.buttons.forEach(function(btnConfig, i){
 
-                // If we don't have Share link in the action bar prevent inserting Buffer button
-                // This usually happens when user is not logged in
-                if ($container.hasClass('buffer-inserted')) {
-                    return;
-                }
+      $(btnConfig.container).each(function(){
 
-                var button = createButton(btnConfig);
+        var $container = $(this);
 
-                var getData = btnConfig.data;
+        // If we don't have Share link in the action bar prevent inserting Buffer button
+        // This usually happens when user is not logged in
+        if ($container.hasClass('buffer-inserted')) {
+          return;
+        }
 
-                $(button).children('a.share_link').on('click', function (e) {
-                    xt.port.emit('buffer_click', getData(button));
-                    e.preventDefault();
-                });
+        var button = createButton(btnConfig);
 
-                $container.addClass('buffer-inserted');
+        var getData = btnConfig.data;
 
-                // We check for the share count
-                var hasShareCount = !!$container.find('.repost_count_link').length;
-                if (!hasShareCount) {
-                    $container.find(btnConfig.after).after(button);
-                    return;
-                }
-
-                // If it has the Downvote, we insert before that element's 
-                // previous element
-                var $before = $container.find(btnConfig.before);
-
-                if ($before.length) {
-                    $before.prev().before(button);
-                    return;
-                }
-
-                $container.append(button);
-            });
-
+        $(button).children('a.share_link').on('click', function (e) {
+          xt.port.emit('buffer_click', getData(button));
+          e.preventDefault();
         });
 
-        setTimeout(insertButtons, 800);
-    };
+        $container.addClass('buffer-inserted');
 
-    ;(function check(){
-        if( xt.options && xt.options['buffer.op.quora'] === 'quora') {
-            insertButtons();
-        } else {
-            setTimeout(check, 50);
+        // We check for the share count
+        var hasShareCount = !!$container.find('.repost_count_link').length;
+        if (!hasShareCount) {
+          $container.find(btnConfig.after).after(button);
+          return;
         }
-    }());
+
+        // If it has the Downvote, we insert before that element's 
+        // previous element
+        var $before = $container.find(btnConfig.before);
+
+        if ($before.length) {
+          $before.prev().before(button);
+          return;
+        }
+
+        $container.append(button);
+      });
+
+    });
+
+    setTimeout(insertButtons, 800);
+  };
+
+  ;(function check(){
+    if( xt.options && xt.options['buffer.op.quora'] === 'quora') {
+      insertButtons();
+    } else {
+      setTimeout(check, 50);
+    }
+  }());
 
 }());
