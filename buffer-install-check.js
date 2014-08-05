79e98c1a522f3ea78cc0b8271554a34f560093e0
Dan Farrelly <daniel.j.farrelly@gmail.com>
2014-08-05 11:25:59 -0400

    Allow install check script to work in chrome.

--- buffer-install-check.js
+++ buffer-install-check.js
@@ -15,3 +15,21 @@ var bufferMarkOurSite = function (version) {
 	}
 };
 
+// Chrome doesn't expose the version so easily with the permissions 
+// we currently require, so we xhr for the manifest file to get the version.
+if (typeof chrome !== 'undefined') {
+
+  function getVersion(callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', chrome.extension.getURL('/manifest.json'));
+    xhr.onload = function (e) {
+      var manifest = JSON.parse(xhr.responseText);
+      callback(manifest.version);
+    }
+    xhr.send(null);
+  }
+ 
+  getVersion(bufferMarkOurSite);
+
+}
+
