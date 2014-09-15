e0d52da39c0c036f1fedfa6ef64a264f29238bdc
Dan Farrelly <daniel.j.farrelly@gmail.com>
2014-09-15 12:34:59 -0400

    Convert indents to tabs.

--- embeds/buffer-reddit.js
+++ embeds/buffer-reddit.js
@@ -1,83 +1,83 @@
 ;(function() {
-		
-	var config = {};
-	config.buttons = [
-		{
-			text: "buffer",
-			container: 'div.entry ul.flat-list',
-			className: 'buffer-reddit-button',
-			selector: '.buffer-reddit-button',
-			data: function (elem) {
-				var article = $(elem).closest('.entry').find('a.title');
-				var title = $(article).text().trim();
-				var link = $(article).attr('href').trim();
-				
-				var image = $(elem).closest('.thing').find('.thumbnail img').attr('src');
+    
+  var config = {};
+  config.buttons = [
+    {
+      text: "buffer",
+      container: 'div.entry ul.flat-list',
+      className: 'buffer-reddit-button',
+      selector: '.buffer-reddit-button',
+      data: function (elem) {
+        var article = $(elem).closest('.entry').find('a.title');
+        var title = $(article).text().trim();
+        var link = $(article).attr('href').trim();
+        
+        var image = $(elem).closest('.thing').find('.thumbnail img').attr('src');
 
-				return {
-					text: title,
-					url: link,
-					image: image,
-					placement: 'reddit-add'
-				};
-			}
-		}
-	];
-	
-	var createButton = function (btnConfig) {
+        return {
+          text: title,
+          url: link,
+          image: image,
+          placement: 'reddit-add'
+        };
+      }
+    }
+  ];
+  
+  var createButton = function (btnConfig) {
 
-		var a = document.createElement('a');
-		a.setAttribute('class', btnConfig.className);
-		a.setAttribute('href', '#');
-		$(a).text(btnConfig.text);
-		
-		var li = document.createElement('li');
-		li.appendChild(a);
+    var a = document.createElement('a');
+    a.setAttribute('class', btnConfig.className);
+    a.setAttribute('href', '#');
+    $(a).text(btnConfig.text);
+    
+    var li = document.createElement('li');
+    li.appendChild(a);
 
-		return li;
+    return li;
 
-	};
+  };
 
-	var insertButtons = function () {
+  var insertButtons = function () {
 
-		var i, l=config.buttons.length;
-		for ( i=0 ; i < l; i++ ) {
+    var i, l=config.buttons.length;
+    for ( i=0 ; i < l; i++ ) {
 
-			var btnConfig = config.buttons[i];
-				
-			$(btnConfig.container).each(function () {
-					
-				var container = $(this);
-				
-				if ( $(container).hasClass('buffer-inserted') ) return;
+      var btnConfig = config.buttons[i];
+        
+      $(btnConfig.container).each(function () {
+          
+        var container = $(this);
+        
+        if ( $(container).hasClass('buffer-inserted') ) return;
 
-				$(container).addClass('buffer-inserted');
+        $(container).addClass('buffer-inserted');
 
-				var btn = createButton(btnConfig);
+        var btn = createButton(btnConfig);
 
-				$(container).append(btn);
-				
-				var getData = btnConfig.data;
+        $(container).append(btn);
+        
+        var getData = btnConfig.data;
 
-				$(btn).click(function (e) {
-					xt.port.emit("buffer_click", getData(btn));
-					e.preventDefault();
-				});
-					
-			});
+        $(btn).click(function (e) {
+          xt.port.emit("buffer_click", getData(btn));
+          e.preventDefault();
+        });
+          
+      });
 
-		}
+    }
 
-	};
+  };
 
-	// Wait for xt.options to be set
-	;(function check() {
-		// If reddit is switched on, add the buttons
-		if( xt.options && xt.options['buffer.op.reddit'] === 'reddit') {
-			insertButtons();
-		} else {
-			setTimeout(check, 2000);
-		}
-	}());
+  // Wait for xt.options to be set
+  ;(function check() {
+    // If reddit is switched on, add the buttons
+    if( xt.options && xt.options['buffer.op.reddit'] === 'reddit') {
+      insertButtons();
+    } else {
+      setTimeout(check, 2000);
+    }
+  }());
 
 }());
