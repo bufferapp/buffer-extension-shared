e9c711a9f00c7751b64f36b9d4fd36239d089036
Dan Farrelly <daniel.j.farrelly@gmail.com>
2015-03-31 18:29:23 -0400

    Add tweet detail view

--- embeds/buffer-tweetdeck.js
+++ embeds/buffer-tweetdeck.js
@@ -1,17 +1,5 @@
 ;(function() {
 
-  // document.body.classList.add('buffer-inserted');
-
-  var $actionItem = $([
-    '<li class="tweet-action-item">',
-      '<a class="js-buffer-action tweet-action position-rel" href="#" rel="buffer">',
-        '<i class="icon icon-buffer txt-right"></i>',
-        '<span class="is-vishidden">Buffer</span> ',
-      '</a>',
-    '</li>'
-  ].join(''));
-
-
   var conifg = [
 
     // Streams
@@ -20,6 +8,49 @@
       selector: '.js-stream-item .js-tweet-actions:not(.buffer-inserted)',
       $button: $([
         '<li class="tweet-action-item">',
+          '<a class="js-buffer-action tweet-action position-rel" href="#" rel="buffer">',
+            '<i class="icon icon-buffer txt-right"></i>',
+            '<span class="is-vishidden">Buffer</span> ',
+          '</a>',
+        '</li>'
+      ].join('')),
+      insert: function(el) {
+        var $actions = $(el);
+        var $newActionItem = this.$button.clone();
+
+        $actions
+          .addClass('buffer-inserted')
+          .prepend($newActionItem);
+
+        return $newActionItem;
+      },
+      getData: function(el) {
+        var $streamItem = $(el).parents('.js-stream-item');
+
+        var $text = $streamItem.find('.js-tweet-text');
+        var $screenname = $streamItem.find('.username');
+        var screenname = $screenname.text().trim().replace(/^@/, '');
+        var text = 'RT @' + screenname + ': ' + $text.text().trim() + '';
+        var displayName = $streamItem.find('.fullname').text().trim();
+
+        return {
+          text: text,
+          placement: this.placement,
+          retweeted_tweet_id: $streamItem.attr('data-key'),
+          // NOTE - we may not really need the user id after all...
+          // retweeted_user_id:           $tweet.attr('data-user-id'),
+          retweeted_user_name: screenname,
+          retweeted_user_display_name: displayName
+        };
+      }
+    },
+
+    // Tweet Detail
+    {
+      placement: 'tweetdeck-detail',
+      selector: '.js-tweet-detail .tweet-detail-actions:not(.buffer-inserted)',
+      $button: $([
+        '<li class="tweet-detail-action-item">',
           '<a class="js-buffer-action tweet-action position-rel" href="#" rel="buffer">',
             '<i class="icon icon-buffer txt-right"></i>',
             '<span class="is-vishidden">Buffer</span> ',
