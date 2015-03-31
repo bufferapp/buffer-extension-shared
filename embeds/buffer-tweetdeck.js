c4bb94e70a6ac53f92ddf0b9cfe0c5909586b202
Dan Farrelly <daniel.j.farrelly@gmail.com>
2015-03-31 18:09:07 -0400

    Refactor to use config list

--- embeds/buffer-tweetdeck.js
+++ embeds/buffer-tweetdeck.js
@@ -11,44 +11,68 @@
     '</li>'
   ].join(''));
 
-  var insertButtons = function() {
 
-    $('.js-stream-item .js-tweet-actions:not(.buffer-inserted)').each(function(i, el) {
-      var $actions = $(el);
-      var $newActionItem = $actionItem.clone();
+  var conifg = [
 
-      $actions
-        .addClass('buffer-inserted')
-        .prepend($newActionItem);
+    // Streams
+    {
+      placement: 'tweekdeck-stream',
+      selector: '.js-stream-item .js-tweet-actions:not(.buffer-inserted)',
+      $button: $([
+        '<li class="tweet-action-item">',
+          '<a class="js-buffer-action tweet-action position-rel" href="#" rel="buffer">',
+            '<i class="icon icon-buffer txt-right"></i>',
+            '<span class="is-vishidden">Buffer</span> ',
+          '</a>',
+        '</li>'
+      ].join('')),
+      insert: function(el) {
+        var $actions = $(el);
+        var $newActionItem = this.$button.clone();
 
-      $newActionItem.on('click', function(e) {
+        $actions
+          .addClass('buffer-inserted')
+          .prepend($newActionItem);
+
+        return $newActionItem;
+      },
+      getData: function(el) {
+        var $streamItem = $(el).parents('.js-stream-item');
+
+        var $text = $streamItem.find('.js-tweet-text');
+        var $screenname = $streamItem.find('.username');
+        var screenname = $screenname.text().trim().replace(/^@/, '');
+        var text = 'RT @' + screenname + ': ' + $text.text().trim() + '';
+        var displayName = $streamItem.find('.fullname').text().trim();
+
+        return {
+          text: text,
+          placement: this.placement,
+          retweeted_tweet_id: $streamItem.attr('data-key'),
+          // NOTE - we may not really need the user id after all...
+          // retweeted_user_id:           $tweet.attr('data-user-id'),
+          retweeted_user_name: screenname,
+          retweeted_user_display_name: displayName
+        };
+      }
+    }
+
+  ];
+
+
+  var insertButton = function(target) {
+    $(target.selector).each(function(i, el) {
+      var $button = target.insert(el);
+      $button.on('click', function(e) {
         e.preventDefault();
-        xt.port.emit('buffer_click', getData( $newActionItem.parents('.js-stream-item') ));
-      });
-
+        xt.port.emit('buffer_click', target.getData(el));
+      })
     });
-
   };
 
-  var getData = function($streamItem) {
-
-    var $text = $streamItem.find('.js-tweet-text');
-    var $screenname = $streamItem.find('.username');
-    var screenname = $screenname.text().trim().replace(/^@/, '');
-    var text = 'RT @' + screenname + ': ' + $text.text().trim() + '';
-    var displayName = $streamItem.find('.fullname').text().trim();
-
-    return {
-      text: text,
-      placement: 'tweetdeck-stream',
-      retweeted_tweet_id: $streamItem.attr('data-key'),
-      // NOTE - we may not really need the user id after all...
-      // retweeted_user_id:           $tweet.attr('data-user-id'),
-      retweeted_user_name: screenname,
-      retweeted_user_display_name: displayName
-    };
+  var insertButtons = function() {
+    conifg.forEach(insertButton);
   };
-
 
   var tweetdeckLoop = function() {
     insertButtons();
