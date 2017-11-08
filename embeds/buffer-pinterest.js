d8baf1aa37044a40678fae1528b1d6beb5f33a4d
Philippe Masset <pioul06+github@gmail.com>
2017-11-08 21:37:21 +0100

    Add support for new Pinterest tlds: .jp, .nz, .com.au

--- embeds/buffer-pinterest.js
+++ embeds/buffer-pinterest.js
@@ -10,7 +10,7 @@
    * - ...
    */
   var hostname = document.location.hostname;
-  if (!/^([^\.]+\.)?pinterest\.(com|pt|de|com\.mx|ie|co\.uk|fr|es|nl|ca|dk|at|se|ch)$/.test(hostname)) return;
+  if (!/^([^\.]+\.)?pinterest\.(com|pt|de|com\.mx|ie|co\.uk|fr|es|nl|ca|dk|at|se|ch|jp|nz|com\.au)$/.test(hostname)) return;
 
   /**
    * Example attribute value (contains both html tags and html entities):
