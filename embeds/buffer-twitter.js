90898ac27a6edb5cdf776860f59c7a17cd407188
Philippe Masset <pioul06+github@gmail.com>
2017-11-08 20:54:34 +0100

    Bring back Buffer button on tweets on Twitter.com

--- embeds/buffer-twitter.js
+++ embeds/buffer-twitter.js
@@ -158,7 +158,7 @@
         'div.tweet-button-sub-container',
         '.tweet-form:not(.dm-tweetbox):not(.RetweetDialog-tweetForm):not(.is-reply) .tweet-button'
       ].join(','),
-      after: '.tweet-counter',
+      before: '.js-tweet-btn',
       className: 'buffer-tweet-button EdgeButton EdgeButton--primary',
       selector: '.buffer-tweet-button',
       create: function (btnConfig) {
@@ -222,15 +222,10 @@
             .find('.tweet-content .tweet-box');
 
         $target.on('keyup focus blur change paste cut', function (e) {
-          var val = $(this).text();
-          var counter = $elem.siblings('.tweet-counter').text() ||
-            $elem.siblings('.tweet-counter').val();
-
-          if ( val.length > 0 && counter > -1 && val !== 'Compose new Tweet...') {
-            $elem.removeClass('disabled');
-          } else {
-            $elem.addClass('disabled');
-          }
+          setTimeout(function() {
+            var isTweetButtonDisabled = $elem.siblings('.js-tweet-btn').is(':disabled');
+            $elem.toggleClass('disabled', isTweetButtonDisabled);
+          }, 0);
         });
       }
     },
@@ -662,7 +657,8 @@
 
         var btn = btnConfig.create(btnConfig);
 
-        $container.find(btnConfig.after).after(btn);
+        if (btnConfig.after) $container.find(btnConfig.after).after(btn);
+        else if (btnConfig.before) $container.find(btnConfig.before).before(btn);
 
         if ( !! btnConfig.activator) btnConfig.activator(btn, btnConfig);
 
